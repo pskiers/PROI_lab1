7e582086ad426a815b349412aa699a77d5218f25
pawel <pawelsk.vp.pl@gmial.com>
2021-03-14 20:03:13 +0100

    made zad 1

---
+++ zad_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double sum, maximum, minimum, new_number;
+    int count = 0;
+    bool check=false;
+
+    cout<<"Enter sequence of numbers. To end the sequence enter something thats not a number (for instance a letter)"<<"\n";
+    do
+    {
+        cin >> new_number;
+        if (cin.fail())
+        {
+            check = true;
+        }
+        else
+        {
+            if (count == 0)
+            {
+                sum = new_number;
+                maximum = new_number;
+                minimum = new_number;
+            }
+            else
+            {
+                sum += new_number;
+                maximum = max(maximum, new_number);
+                minimum = min(minimum, new_number);
+
+            }
+            count ++;
+        }
+    } while (check == false);
+    if (count == 0)
+    {
+        cout<<"Recived no number";
+    }
+    else
+    {
+        cout<<sum/count<<" = avarage"<<"\n";
+        cout<<maximum<<" = maximum"<<"\n";
+        cout<<minimum<<" = minimum"<<"\n";
+    }
+return 0;
+}
